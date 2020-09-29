79c849d2ff891358faa3c9ba39fa48fd196d8e27
Chetan77verma <prem.premsharma77@gmail.com>
2020-09-29 15:43:34 +0530

    starter project

---
+++ src/routes/about.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<h1>About Job Ninja</h1>
+
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus corrupti
+	nihil velit sit pariatur harum reprehenderit voluptatibus labore nostrum,
+	vitae culpa, distinctio libero maxime similique debitis repudiandae eveniet
+	assumenda accusamus.
+</p>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus corrupti
+	nihil velit sit pariatur harum reprehenderit voluptatibus labore nostrum,
+	vitae culpa, distinctio libero maxime similique debitis repudiandae eveniet
+	assumenda accusamus.
+</p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,40 @@
+<style>
+	h1,
+	p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	div {
+		text-align: center;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Job Ninja</title>
+</svelte:head>
+
+<div>
+	<img src="successkid.jpg" alt="logo" />
+	<h1>Find new Jobs</h1>
+	<p>Find your new assignments here</p>
+	<a href="jobs">View jobs</a>
+</div>
